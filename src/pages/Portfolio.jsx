e66610f1daa281fa9b986ba654fc9110258e1740
reacali <caliendoreagan@gmail>
2025-05-22 14:34:30 -0500

    added postcards to portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import portfolioPostcard from '../assets/images/portfolio_postcard.png';
+import fridgePostcard from '../assets/images/fridge_postcard.png';
+import sigcyberPostcard from '../assets/images/sigcyber_postcard.png';
+import cryptoPostcard from '../assets/images/crypto_postcard.png';
+import '../styles/Portfolio.css'; // Import your CSS file for styling
 
 function Portfolio() {
     return (
-        <div>
-            <h1>in progress</h1>
+        <div className="postcards">
+            <div className="postcard">
+                <img src={portfolioPostcard} alt="Portfolio Postcard" className="portfolio-postcard-image" />
+                <p className="postcard-text">Portfolio Project</p>
+            </div>
+            <div className="postcard">
+                <img src={fridgePostcard} alt="Fridge Postcard" className="portfolio-postcard-image" />
+                <p className="postcard-text">Fridge Management App</p>
+            </div>
+            <div className="postcard">
+                <img src={sigcyberPostcard} alt="SigCyber Postcard" className="portfolio-postcard-image" />
+                <p className="postcard-text">ACM SIG Cyber</p>
+            </div>
+            <div className="postcard">
+                <img src={cryptoPostcard} alt="Crypto Postcard" className="portfolio-postcard-image" />
+                <p className="postcard-text">Cryptocurrency Sentiment Analysis</p>
+            </div>
         </div>
     );
 }
